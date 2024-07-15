5d03f2947d618a4e3093bf9de4e22d9cc7a68a3e
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-07-15 20:58:42 +0700

    ui: dashed-placeholder

--- app/03-approach/page.tsx
+++ app/03-approach/page.tsx
@@ -3,6 +3,7 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/com
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
 import { productColumns, productRows } from "@/data/products";
 import { Icons } from "@/components/icons";
+import DashedPlaceholder from "@/components/ui/dashed-placeholder";
 
 export default function ThirdApproachPage() {
     return (
@@ -25,7 +26,7 @@ export default function ThirdApproachPage() {
                             {productRows.map((product) => (
                                 <TableRow key={product.name}>
                                     <TableCell>
-                                        <ImagePlaceholder />
+                                        <DashedPlaceholder />
                                     </TableCell>
                                     <TableCell className="font-medium">{product.name}</TableCell>
                                     <TableCell>{product.category}</TableCell>
@@ -50,32 +51,5 @@ export default function ThirdApproachPage() {
                 </CardContent>
             </Card>
         </>
-    );
-}
-
-function ImagePlaceholder() {
-    return (
-        <div className="relative h-14 aspect-square overflow-hidden rounded-xl border border-dashed border-gray-400 opacity-75">
-            <svg className="absolute inset-0 h-full w-full stroke-gray-900/10" fill="none">
-                <defs>
-                    <pattern
-                        id="pattern-1526ac66-f54a-4681-8fb8-0859d412f251"
-                        x="0"
-                        y="0"
-                        width="10"
-                        height="10"
-                        patternUnits="userSpaceOnUse"
-                    >
-                        <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3"></path>
-                    </pattern>
-                </defs>
-                <rect
-                    stroke="none"
-                    fill="url(#pattern-1526ac66-f54a-4681-8fb8-0859d412f251)"
-                    width="100%"
-                    height="100%"
-                ></rect>
-            </svg>
-        </div>
     );
 }
--- components/ui/database-modal/new-database-modal.tsx
+++ components/ui/database-modal/new-database-modal.tsx
@@ -1,21 +1,13 @@
-import { DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+import { DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import DashedPlaceholder from "@/components/ui/dashed-placeholder";
 
 export default function NewDatabaseModal() {
     return (
-        <DialogContent>
+        <DialogContent className="w-[400px] h-[450px]">
             <DialogHeader>
                 <DialogTitle>New Database</DialogTitle>
             </DialogHeader>
-            <DialogFooter>
-                <div>
-                    <Button variant="outline">Cancel</Button>
-                </div>
-                <div className="space-x-2">
-                    <Button variant="outline">New...</Button>
-                    <Button>Confirm</Button>
-                </div>
-            </DialogFooter>
+            <DashedPlaceholder />
         </DialogContent>
     );
 }
