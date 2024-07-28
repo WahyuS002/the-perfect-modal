4f8379897741b9ac123f1a6fc8d09437c53cb354
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-07-28 09:43:12 +0700

    feat: changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
+const links = [
+    { href: "/01-approach", title: "01 Approach" },
+    { href: "/02-approach", title: "02 Approach" },
+    { href: "/03-approach", title: "03 Approach" },
+    { href: "/04-approach", title: "04 Approach" },
+];
+
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center space-y-4">
             <h1 className="text-4xl font-bold">The Perfect Modal</h1>
             <p className="text-lg">Learn to implement the perfect modal in react.js @pixel.developer</p>
+            {links.map((link) => (
+                <Link
+                    href={link.href}
+                    className="bg-zinc-50 border border-black/10 px-10 py-2 rounded-md hover:bg-white cursor-pointer group"
+                >
+                    <span className="text-muted-foreground group-hover:text-black">{link.title}</span>
+                </Link>
+            ))}
         </div>
     );
 }
--- components/ui/current-page.tsx
+++ components/ui/current-page.tsx
@@ -6,9 +6,13 @@ export default function CurrentPage() {
     const currentPage = pathname.split("/")[1];
 
     return (
-        <div className="flex items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 shadow-sm text-xs absolute top-4 left-4 space-x-1">
-            <span className="text-muted-foreground">Current Page:</span>
-            <span>{currentPage}</span>
-        </div>
+        <>
+            {currentPage && (
+                <div className="flex items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 shadow-sm text-xs absolute top-4 left-4 space-x-1">
+                    <span className="text-muted-foreground">Current Page:</span>
+                    <span>{currentPage}</span>
+                </div>
+            )}
+        </>
     );
 }
